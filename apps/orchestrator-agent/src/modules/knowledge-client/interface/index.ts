85311f064e6aad9c1adbb05dda17cd4c20c5a502
Yeoh Yong Shan <yeohyongshan@gmail.com>
2026-03-22 13:45:47 +0800

    Added implementation of GetRecentOrders

---
+++ apps/orchestrator-agent/src/modules/knowledge-client/interface/index.ts
@@ -0,0 +1,4 @@
+export { SearchFaqPayload } from "./search-faq-payload.interface";
+export { SearchSopPayload } from "./search-sop-payload.interface";
+export { SearchFaqResponse } from "./search-faq-response.interface";
+export { SearchSopResponse } from "./search-sop-response.interface";
